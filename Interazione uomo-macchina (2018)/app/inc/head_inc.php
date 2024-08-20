8371d67b1f42a2c28f0bb33d14835a405f70781f
Gian Marco Ninno <113545072+ningia92@users.noreply.github.com>
2024-08-20 17:42:47 +0200

    Delete Interazione uomo-macchina (2018) directory

--- Interazione uomo-macchina (2018)/app/inc/head_inc.php
+++
@@ -1,5 +0,0 @@
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="<?php echo CSS_DIR;?>bootstrap.min.css">
-    <script src="<?php echo JS_DIR;?>jquery.min.js"></script>
-    <script src="<?php echo JS_DIR;?>bootstrap.min.js"></script>
--- Interazione uomo-macchina (2018)/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require_once 'lib/config.php';
-    if(! isUserLoggedIn()) {
-        header("Location: " .ACCOUNT_DIR."landing-page.php");
-    } else {
-        header("Location: " .ACCOUNT_DIR."login.php");
-    }
-?>
